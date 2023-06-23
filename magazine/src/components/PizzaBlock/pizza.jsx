356726b3c48db9ffe26df610873782dc8606d5bd
Alexander Ershov <planyolan@gmail.com>
2023-06-23 17:55:15 +0300

    Новая ветка

--- magazine/src/components/PizzaBlock/pizza.jsx
+++ magazine/src/components/PizzaBlock/pizza.jsx
@@ -6,40 +6,42 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
   const [activeSize, setActiveSize] = React.useState(0);
   const typeNames = ["тонкое", "традиционное"];
   return (
-    <div className="pizza-block">
-      <img className="pizza-block__image" alt="Pizza" src={imageUrl} />
-      <h4 className="pizza-block__title">{title}</h4>
-      <div className="pizza-block__selector">
-        <ul>
-          {types.map((typeId) => (
-            <li
-              key={typeId}
-              onClick={() => setActiveType(typeId)}
-              className={activeType === typeId ? "active" : ""}
-            >
-              {typeNames[typeId]}{" "}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {sizes.map((size, i) => (
-            <li
-              key={size}
-              onClick={() => setActiveSize(i)}
-              className={activeSize === i ? "active" : ""}
-            >
-              {size} см.
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от {price}P</div>
-        <button className="button button--outline button--add">
-          <img width='12' src={orangePlus} alt="#" />
-          <span>Добавить</span>
-          <i>0</i>
-        </button>
+    <div className="pizza-block-wrapper">
+      <div className="pizza-block">
+        <img className="pizza-block__image" alt="Pizza" src={imageUrl} />
+        <h4 className="pizza-block__title">{title}</h4>
+        <div className="pizza-block__selector">
+          <ul>
+            {types.map((typeId) => (
+              <li
+                key={typeId}
+                onClick={() => setActiveType(typeId)}
+                className={activeType === typeId ? "active" : ""}
+              >
+                {typeNames[typeId]}{" "}
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {sizes.map((size, i) => (
+              <li
+                key={size}
+                onClick={() => setActiveSize(i)}
+                className={activeSize === i ? "active" : ""}
+              >
+                {size} см.
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="pizza-block__bottom">
+          <div className="pizza-block__price">от {price}P</div>
+          <button className="button button--outline button--add">
+            <img width='12' src={orangePlus} alt="#" />
+            <span>Добавить</span>
+            <i>0</i>
+          </button>
+        </div>
       </div>
     </div>
   );
--- magazine/src/pages/Home.jsx
+++ magazine/src/pages/Home.jsx
@@ -18,18 +18,18 @@ export const Home = () => {
       });
   }, []);
   return (
-    <>
-    <div className="content__top">
+    <div className='container'>
+      <div className="content__top">
             <Categories />
             <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
+      </div>
+        <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {isLoading
               ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
               : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
-    </>
+    </div>
   );
 }
 
